57312453c6bde498743f694730f7182bcd12d6f1
Maccio92 <valerio362@gmail.com>
2022-02-11 18:00:15 +0100

    add footer

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -27,4 +27,28 @@
         </div>
     </div>  
 </div>
+<div class="second-container">
+        <div class="container">
+            <div class="product">
+                <img class="img" src="{{asset('../../../img/buy-comics-digital-comics.png')}}">
+                <p class="caption">DIGITAL COMICS</p>
+            </div>
+            <div class="product">
+                <img class="img" src="../../../img/buy-comics-merchandise.png">
+                <p class="caption">DC MERCHANDISE</p>
+            </div>
+            <div class="product">
+                <img class="img" src="../../../img/buy-comics-subscriptions.png">
+                <p class="caption">SUBSCRIPTIONS</p>
+            </div>
+            <div class="product">
+                <img class="img" src="../../../img/buy-comics-shop-locator.png">
+                <p class="caption">DC SHOP LOCATOR</p>
+            </div>
+            <div class="product">
+                <img class="img" src="../../../img/buy-dc-power-visa.svg">
+                <p class="caption">DC POWER VISA</p>
+            </div>
+        </div>
+    </div>
 @endsection
---
+++ resources/views/guest/partials/footer.blade.php
@@ -0,0 +1,66 @@
+<div>
+    <div class="upper-footer ">
+        <div class="container-footer ">
+            <div class="col">
+                <h3>DC COMICS</h3>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <h3>SHOP</h3>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>DC</h3>
+                <ul>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy</li>
+                    <li>Ad Choice</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>SITES</h3>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+            <img class="thumb-bg" src="../../../img/dc-logo-bg.png" alt="">
+        </div>
+    </div>
+    <div class="lower-footer">
+        <div class="container-footer container-low">
+            <div class="left-col">
+                <button>SIGN-UP NOW</button>
+            </div>
+            <div class="right-col ">
+                <h2>FOLLOW US</h2>
+                <img src="../../img/footer-facebook.png" alt="">
+                <img src="../../img/footer-twitter.png" alt="">
+                <img src="../../img/footer-youtube.png" alt="">
+                <img src="../../img/footer-pinterest.png" alt="">
+                <img src="../../img/footer-periscope.png" alt="">
+            </div>
+
+        </div>
+    </div>
+
+</div>
