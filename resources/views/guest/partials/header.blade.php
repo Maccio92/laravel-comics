ec0d566f04255a4d042b28565d0936e569c8bfe4
Maccio92 <valerio362@gmail.com>
2022-02-11 16:05:46 +0100

    add header

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -2,8 +2,15 @@
 <div class="header-container">
     <img src="{{asset('/img/dc-logo.png')}}" alt="Logo">
     <ul class="nav">
-        
+        <li><a href="#">comics</a></li>
+        <li><a href="#">movies</a></li>
+        <li><a href="#">tv</a></li>
+        <li><a href="#">games</a></li>
+        <li><a href="#">collectibles</a></li>
+        <li><a href="#">videos</a></li>
+        <li><a href="#">fans</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">shop</a></li>
     </ul>
 </div>
 </header>
-
